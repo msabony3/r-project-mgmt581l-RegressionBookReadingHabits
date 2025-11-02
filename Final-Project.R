bee20f8c0dcdded45761ada634db4bd9b133b461
Matt Sabony <msabony3@gmail.com>
2025-11-02 12:22:01 -0500

    Add files via upload

---
+++ Final-Project.R
@@ -0,0 +1,6 @@
+movie_metadata <- read.csv("movies_metadata.csv") # Posters, backdrop, budget, revenue, release dates, languages, country, production company
+credits <- read.csv("credits.csv") # Cast and crew
+keywords <- read.csv("keywords.csv") # Plot keywords
+ratings_small <- read.csv("ratings_small.csv") # Ratings
+
+# Question: 
